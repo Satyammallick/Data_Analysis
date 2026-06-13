6d2c269b8be5d04551ab6b7b5bc467db30c5fcae
Satyam Malik <147387663+Satyammallick@users.noreply.github.com>
2026-06-13 19:49:28 +0530

    Add files via upload

--- sql3.sql
+++ sql3.sql
@@ -2,15 +2,43 @@ CREATE DATABASE google;
 
 use google;
 
+
 CREATE TABLE employee(
 id INT PRIMARY KEY,
-name VARCHAR(50),
+dept VARCHAR(50),
 salary INT
 );
 
 INSERT INTO employee 
-VALUES (1,"Adam",25000),
-	   (2,"Bob",30000),
-       (3,"Casey",40000);
+VALUES (1,"Sales",25000),
+	   (2,"HR",30000),
+       (3,"HR",40000),
+       (4,"Sales",15000);
        
        SELECT *FROM employee;
+       
+        
+        #GROUP_BY
+        
+       SELECT dept,AVG(salary) 
+       FROM employee
+       GROUP BY dept; 
+       
+       
+       
+       
+       
+       SELECT *,
+       AVG(salary) OVER(PARTITION BY dept) AS dept_avg_sal
+       FROM employee;
+       
+       
+       
+       
+       
+       #WINDOW_FUNCTIONS
+       
+       SELECT id,dept,salary,
+       AVG(salary) OVER(PARTITION BY dept) AS dept_avg_sal 
+       FROM employee;
+       
