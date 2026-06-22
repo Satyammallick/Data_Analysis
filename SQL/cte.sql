20e2ec2b6f406728545524bf071013bf8941fc65
Satyam Malik <147387663+Satyammallick@users.noreply.github.com>
2026-06-22 23:03:54 +0530

    Create sales table and average sales query
    Added sales table and query to find stores with sales above average.

--- SQL/cte.sql
+++ SQL/cte.sql
@@ -53,7 +53,57 @@ VALUES (1,"Sales",25000),
       SELECT *
       FROM employee e , average_salary a
       WHERE salary > a.avg_sal ;
+
+
+
+
+    ***********Another Example Of WITH_CLAUSE()----------------
        
+
+CREATE TABLE sales(
+	store_id  		INT,
+	store_name  	VARCHAR(50),
+	product			VARCHAR(50),
+	quantity		INT,
+	cost			INT
+);
+INSERT INTO sales VALUES
+(1, 'Apple Originals 1','iPhone 12 Pro', 1, 1000),
+(1, 'Apple Originals 1','MacBook pro 13', 3, 2000),
+(1, 'Apple Originals 1','AirPods Pro', 2, 280),
+(2, 'Apple Originals 2','iPhone 12 Pro', 2, 1000),
+(3, 'Apple Originals 3','iPhone 12 Pro', 1, 1000),
+(3, 'Apple Originals 3','MacBook pro 13', 1, 2000),
+(3, 'Apple Originals 3','MacBook Air', 4, 1100),
+(3, 'Apple Originals 3','iPhone 12', 2, 1000),
+(3, 'Apple Originals 3','AirPods Pro', 3, 280),
+(4, 'Apple Originals 4','iPhone 12 Pro', 2, 1000),
+(4, 'Apple Originals 4','MacBook pro 13', 1, 2500);
+
+SELECT * FROM sales;
+       
+---(Q). Find stores whose sales are more than the average sales across all stores.
+
+#1. Total sales per each store----> total_sales
+
+#2. Find the average sales with respect to all the stores ---> avg_sales
+
+#3. Find the Stores where (total_sales > agv_sales of all stores)
+
+
+    WITH
+    total_sales AS (
+    SELECT s.store_id , SUM(s.cost) AS total_sales_per_store
+    FROM sales s
+    GROUP BY s.store_id ),
+    avg_sales AS (
+    SELECT AVG(total_sales_per_store)  AS avg_sales_for_each_store
+    FROM total_sales )
+    
+SELECT * 
+FROM total_sales t
+JOIN avg_sales a
+ON t.total_sales_per_store > a.avg_sales_for_each_store;
        
     
        
